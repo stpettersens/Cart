7255517f9fa9d7540b3499d611f80d4858cd27e1
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-10 22:21:39 +0100

    Cookie works fine; need to fix localStorage.

--- configure-cart.js
+++ configure-cart.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   // Don't set a custom name for cart.
-  // Don't force use of cookie instead of localStorage.
+  // Force use of cookie instead of localStorage.
   // Use $ for currency.
   // Use bootstrap for table and buttons.
   // Use striped boostrap table.
   // Use alerts.
-  Cart.configure(null, false, '$', true, true, true);
+  Cart.configure(null, true, '$', true, true, true);
 });
